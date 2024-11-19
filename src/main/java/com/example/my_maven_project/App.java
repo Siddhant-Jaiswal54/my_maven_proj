e14260994fa24475d1e689dadbb42f41a4545d6c
Siddhant-Jaiswal54 <dsiddhantjaiswal@gmail.com>
2024-11-19 22:14:41 +0530

    initial commit

---
+++ src/main/java/com/example/my_maven_project/App.java
@@ -0,0 +1,10 @@
+package com.example.my_maven_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is Siddhant Jaiswal (DSE31) 22BD1A6754");
+    }
+}
